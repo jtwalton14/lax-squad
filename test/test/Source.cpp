dcb57b44adf7728994db0d1f28d3048fc41b22b3
Austin Marczak <marczaka@findlay.edu>
2020-03-09 17:43:21 -0400

    Logic of changing to black and white

--- test/test/Source.cpp
+++ test/test/Source.cpp
@@ -3,16 +3,22 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace cv;
 using namespace std;
 
-string HueName[] = { "Red", "Yellow", "Green", "Blue", "Pink" };
-int HueValue[] = { 180, 30, 60, 120, 150 };
+vector <string> colors = { "Red", "Yellow", "Green", "Blue", "Pink" };
+vector <int> hueVal = { 180, 30, 60, 120, 150 };
 
-unsigned char blueShift(unsigned char h);
-unsigned char greenShift(unsigned char h);
 unsigned char redShift(unsigned char h);
+unsigned char yellowShift(unsigned char h);
+unsigned char greenShift(unsigned char h);
+unsigned char lightBlueShift(unsigned char h);
+unsigned char darkBlueShift(unsigned char h);
+unsigned char pinkShift(unsigned char h);
+unsigned char blackShift(unsigned char l);
+unsigned char whiteShift(unsigned char l);
 
 int main()
 {
@@ -20,8 +26,10 @@ int main()
 	//Mat img = imread("flower.jpeg");
 	//Mat img = imread("testPic.jpg");
 	//Mat img = imread("mesh.jpg");
-	Mat img = imread("colorNetTest.png");
-	//Mat img = imread("rgb.jfif");
+	//Mat img = imread("colorNetTest.png");
+	//Mat img = imread("rgb.jpg");
+	//Mat img = imread("threeColorNet.jpg");
+	Mat img = imread("orange_blackNet.jpg");
 
 	Mat hsv;
 	cvtColor(img, hsv, COLOR_BGR2HSV);
@@ -34,19 +42,44 @@ int main()
 			// Saturation is hsv.at<Vec3b>(j, i)[1], and 
 			// Value is hsv.at<Vec3b>(j, i)[2].
 			unsigned char h = hsv.at<Vec3b>(j, i)[0];
+			unsigned char s = hsv.at<Vec3b>(j, i)[1];
+			unsigned char l = hsv.at<Vec3b>(j, i)[2];
 
-			if (h < 30 || h > 150) {
+			/*if (someVariable = black){
+			
+				s = 0;
+				l = blackShift(l);
+			
+			}
+			else if (someVariable = white){
+			
+				s = 0;
+				l = whiteShift(l);
+			
+			}*/
+			/*else*/ if ((h < 15 || h > 165) && s > 30){
 				h = redShift(h);
 			}
-			else if (h < 90 && h > 30) {
+			else if (h < 45 && h > 15) {
+				h = yellowShift(h);
+			}
+			else if (h < 75 && h > 45) {
 				h = greenShift(h);
 			}
-			else if (h < 150 && h > 90) {
-				h = blueShift(h);
+			else if (h < 105 && h > 75) {
+				h = lightBlueShift(h);
+			}
+			else if (h < 135 && h > 105) {
+				h = darkBlueShift(h);
+			}
+			else if (h < 165 && h > 135) {
+				h = pinkShift(h);
 			}
 
 			// Set hue.
 			hsv.at<Vec3b>(j, i)[0] = h;
+			hsv.at<Vec3b>(j, i)[1] = s;
+			hsv.at<Vec3b>(j, i)[2] = l;
 		}
 	}
 
@@ -59,11 +92,22 @@ int main()
 	return 0;
 }
 
-unsigned char blueShift(unsigned char h) {
-	const unsigned char hue_shift = 150;
+unsigned char redShift(unsigned char h) {
+	const unsigned char hue_shift = 0;
 
-	if (h + hue_shift > 120)
-		h = (h + hue_shift) - 120;
+	if (h + hue_shift > 180)
+		h = (h + hue_shift) - 180;
+	else
+		h = h + hue_shift;
+
+	return h;
+}
+
+unsigned char yellowShift(unsigned char h) {
+	const unsigned char hue_shift = 0;
+
+	if (h + hue_shift > 30)
+		h = (h + hue_shift) - 30;
 	else
 		h = h + hue_shift;
 
@@ -71,7 +115,7 @@ unsigned char blueShift(unsigned char h) {
 }
 
 unsigned char greenShift(unsigned char h) {
-	const unsigned char hue_shift = 30;
+	const unsigned char hue_shift = 0;
 
 	if (h + hue_shift > 60)
 		h = (h + hue_shift) - 60;
@@ -81,13 +125,63 @@ unsigned char greenShift(unsigned char h) {
 	return h;
 }
 
-unsigned char redShift(unsigned char h) {
-	const unsigned char hue_shift = 120;
+unsigned char lightBlueShift(unsigned char h) {
+	const unsigned char hue_shift = 0;
 
-	if (h + hue_shift > 180)
-		h = (h + hue_shift) - 180;
+	if (h + hue_shift > 90)
+		h = (h + hue_shift) - 90;
 	else
 		h = h + hue_shift;
 
 	return h;
+}
+
+unsigned char darkBlueShift(unsigned char h) {
+	const unsigned char hue_shift = 0;
+
+	if (h + hue_shift > 120)
+		h = (h + hue_shift) - 120;
+	else
+		h = h + hue_shift;
+
+	return h;
+}
+
+unsigned char pinkShift(unsigned char h) {
+	const unsigned char hue_shift = 0;
+
+	if (h + hue_shift > 150)
+		h = (h + hue_shift) - 150;
+	else
+		h = h + hue_shift;
+
+	return h;
+}
+
+unsigned char blackShift(unsigned char l) {
+
+	const unsigned char light_shift = 150;
+	
+	if (l - light_shift <= 0) {
+		l = 0;
+	}
+	else {
+		l -= light_shift;
+	}
+
+	return l;
+}
+
+unsigned char whiteShift(unsigned char l) {
+
+	const unsigned char light_shift = 0;
+
+	if (l + light_shift >= 255) {
+		l = 255;
+	}
+	else {
+		l += light_shift;
+	}
+
+	return l;
 }
